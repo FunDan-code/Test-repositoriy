80e0abb948045885c42caefe58b95628085e23e4
fundan <prodanikyt777@gmail.com>
2025-10-19 17:46:36 +0300

    First commit

---
+++ BallsForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace GravityBalls
+{
+	public class BallsForm : Form
+	{
+		private Timer timer;
+		private WorldModel world;
+		private Font font;
+
+		private WorldModel CreateWorldModel()
+		{
+			var w = new WorldModel
+			{
+				WorldHeight = ClientSize.Height,
+				WorldWidth = ClientSize.Width,
+				BallRadius = 10
+			};
+			w.PlayerX = w.WorldWidth - 40; //местоположение ракетки игрока
+            w.PlayerY = w.WorldHeight / 2;
+            w.EnemyX = w.EnemyWidth + 2;
+            w.EnemyY = w.EnemyHeight / 2;
+            w.BallX = w.WorldHeight / 2;
+			w.BallY = w.BallRadius;
+			return w;
+		}
+
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+            world.PlayerX = world.WorldWidth - 40;
+            world.PlayerY = world.WorldHeight / 2;
+            world.EnemyX = world.EnemyWidth /  2;
+            world.WorldHeight = ClientSize.Height;
+			world.WorldWidth = ClientSize.Width;
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			DoubleBuffered = true;
+			BackColor = Color.Black;
+			world = CreateWorldModel();
+			timer = new Timer { Interval = 30 };
+			timer.Tick += TimerOnTick;
+			timer.Start();
+			world.WorldHeight = ClientSize.Height;
+			world.WorldWidth = ClientSize.Width;
+
+			font = new Font(FontFamily.GenericSerif, 12.0F);
+		}
+
+		private void TimerOnTick(object sender, EventArgs eventArgs)
+		{
+			world.SimulateTimeframe(timer.Interval / 1000d);
+			Invalidate();
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			var g = e.Graphics;
+			g.SmoothingMode = SmoothingMode.HighQuality;
+
+			var points = string.Format("{0} | {1}", world.EnemyScore, world.PlayerScore);
+			g.DrawString(points, font, Brushes.White, (float)world.WorldWidth / 2, (float)10);
+
+			//Отрисовка ракетки врага по заданным свойствам
+			g.FillRectangle(Brushes.Blue,
+                (float)(world.PlayerX),
+                (float)(world.PlayerY - 40),
+                (float)world.PlayerWidth,
+                (float)world.PlayerHeight);
+
+            g.FillRectangle(Brushes.Red,
+                (float)(world.EnemyX),
+                (float)(world.EnemyY - 2),
+                (float)world.EnemyWidth,
+                (float)world.EnemyHeight);
+
+            g.FillEllipse(Brushes.White,
+				(float)(world.BallX - world.BallRadius),
+				(float)(world.BallY - world.BallRadius),
+				2 * (float)world.BallRadius,
+				2 * (float)world.BallRadius);
+		}
+
+		protected override void OnMouseMove(MouseEventArgs e)
+		{
+			base.OnMouseMove(e);
+			Text = string.Format("Cursor ({0}, {1})", e.X, e.Y);
+		}
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BallsForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "BallsForm";
+            this.ResumeLayout(false);
+
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.KeyCode == Keys.W)
+            {
+                world.PlayerY -= world.PlayerSpeed;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                world.PlayerY += world.PlayerSpeed;
+            }
+        }
+    }
+}
---
+++ WorldModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.CodeDom;
+using System.Windows.Forms;
+
+namespace GravityBalls
+{
+	public class WorldModel
+	{
+		public double BallX;
+		public double BallY;
+		public double BallVx = 300;
+        public double BallVy = 300;
+        public double BallRadius;
+		public double WorldWidth;
+		public double WorldHeight;
+		public double R = 0.2; //Сила сопротивления
+		public double G = 500; //Сила притяжения
+		public double F = 30000; //Сила отталкивания от курсора
+
+		//Параметры игрока
+		public double PlayerX;
+        public double PlayerY;
+		public double PlayerHeight = 80;
+		public double PlayerWidth = 20;
+		public double PlayerSpeed = 25;
+		public int PlayerScore = 0;
+
+        //Параметры противника
+        public double EnemyX;
+        public double EnemyY;
+        public double EnemyHeight = 80;
+        public double EnemyWidth = 20;
+        public double EnemySpeed = 25;
+        public int EnemyScore = 0;
+
+        public void SimulateTimeframe(double dt)
+		{
+			MoveBall(dt);
+			ApplyWallsBouncing();
+			ApplyPlayerBouncing(dt);
+			ApplyEnemyBouncing(dt);
+			ApplyEnemyMove();
+			ApplyPointsCount();
+            //ApplyAirResistance(dt);
+
+            //ApplyG(dt);
+            //ApplyF(dt);
+        }
+
+		private void ApplyPlayerBouncing(double dt)
+		{
+			if (BallX + BallRadius >= PlayerX && BallY + BallRadius >= PlayerY && BallY - BallRadius <= PlayerY + PlayerHeight)
+			{
+				BallVx = -BallVx;
+			}
+		}
+
+        private void ApplyEnemyBouncing(double dt)
+        {
+            if (BallX - BallRadius <= EnemyX + EnemyWidth && BallY + BallRadius >= EnemyY && BallY - BallRadius <= EnemyY + EnemyHeight)
+            {
+                BallVx = -BallVx;
+            }
+        }
+
+		private void ApplyEnemyMove()
+		{
+			if (EnemyY + EnemyHeight / 2 < BallY)
+			{
+				EnemyY += EnemySpeed;
+			}
+            if (EnemyY + EnemyHeight / 2 > BallY)
+            {
+				EnemyY -= EnemySpeed;
+			}
+		}
+
+		private void ApplyPointsCount()
+		{
+			if(BallX - BallRadius == 0) //Левая стена - балл нам
+			{
+				PlayerScore++;
+			}
+            if (BallX + BallRadius == WorldWidth) //Правая стена - балл противнику
+            {
+                EnemyScore++;
+            }
+
+        }
+
+        private void MoveBall(double dt)
+		{
+			BallX += BallVx * dt;
+			BallY += BallVy * dt;
+
+			BallX = Math.Max(BallRadius, Math.Min(BallX, WorldWidth- BallRadius));
+            BallY = Math.Max(BallRadius, Math.Min(BallY, WorldHeight - BallRadius));
+        }
+
+		private void ApplyWallsBouncing()
+		{
+			if (BallX - BallRadius <= 0 || BallX + BallRadius >= WorldWidth)
+			{
+				BallVx *=-1;
+			}
+            if (BallY - BallRadius <= 0 || BallY + BallRadius >= WorldHeight)
+			{
+				BallVy *=-1;
+			}
+
+        }
+
+		private void ApplyAirResistance(double dt)
+		{
+			BallVx -= BallVx * R *dt;
+            BallVy -= BallVy * R * dt;
+
+
+        }
+		private void ApplyG(double dt)
+		{
+			BallVy += G * dt;
+		}
+		private void ApplyF(double dt)
+		{
+			var cursorX = Cursor.Position.X;
+			var cursorY = Cursor.Position.Y;
+			var dx = BallX - cursorX;
+			var dy = BallY - cursorY;
+			var d = Math.Sqrt(dx * dx + dy * dy);
+			var f = F/ (d*d);
+			BallVx += dx * f * dt;
+			BallVy += dy * f * dt;
+		}
+	}
+}
